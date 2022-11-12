81838d587f24124ad0820811017bd4d93253c802
Mayke Santos <maykedesenvolvimento@gmail.com>
2022-11-12 20:54:25 -0300

    saving devicesv

--- lib/device/details_page.dart
+++ lib/device/details_page.dart
@@ -1,26 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:localstorage/localstorage.dart';
 
-class DeviceDetailsPage extends StatelessWidget {
+class DeviceDetailsPage extends StatefulWidget {
   final BluetoothDevice device;
+  final Map<String, String> deviceNames;
+  final Function(String) onChangeDeviceName;
 
-  const DeviceDetailsPage({super.key, required this.device});
+  const DeviceDetailsPage({
+    Key? key,
+    required this.device,
+    required this.deviceNames,
+    required this.onChangeDeviceName,
+  }) : super(key: key);
+
+  @override
+  State<DeviceDetailsPage> createState() => _DeviceDetailsPageState();
+}
+
+class _DeviceDetailsPageState extends State<DeviceDetailsPage> {
+  final LocalStorage storage = LocalStorage('bluetooth');
+
+  late String? deviceName;
+  late bool saved;
+  bool connected = false;
+
+  void saveDeviceName() async {
+    await storage.ready;
+    widget.onChangeDeviceName(deviceName!);
+    setState(() => saved = true);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    deviceName = widget.deviceNames[widget.device.id.id];
+    saved = deviceName != null;
+    widget.device.state.listen((state) {
+      setState(() {
+        connected = state == BluetoothDeviceState.connected;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(device.name),
+        title: Text(saved ? deviceName! : 'Dispositivo desconhecido'),
       ),
+      floatingActionButton: connected
+          ? FloatingActionButton(
+              onPressed: widget.device.disconnect,
+              child: const Icon(Icons.bluetooth_disabled),
+            )
+          : FloatingActionButton(
+              onPressed: widget.device.connect,
+              child: const Icon(Icons.bluetooth_connected),
+            ),
       extendBody: true,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(device.name),
-          Text(device.id.toString()),
-          Text(device.type.name),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            saved
+                ? Text('Nome: $deviceName')
+                : Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Nome do dispositivo',
+                            ),
+                            onChanged: (value) {
+                              setState(() {
+                                deviceName = value;
+                              });
+                            },
+                          ),
+                        ),
+                        Container(
+                          width: 100,
+                          margin: const EdgeInsets.only(left: 10),
+                          height: 60,
+                          child: ElevatedButton(
+                            onPressed: saveDeviceName,
+                            child: const Text('Salvar'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+            Text(widget.device.id.toString()),
+            Text(widget.device.type.name),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Conectado: '),
+                Text(
+                  connected ? 'Sim' : 'Não',
+                  style: TextStyle(
+                    color: connected ? Colors.green : Colors.red,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/device/listing_page.dart
+++ lib/device/listing_page.dart
@@ -1,6 +1,7 @@
 import 'package:bluetooth/device/details_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:localstorage/localstorage.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class DeviceListingPage extends StatefulWidget {
@@ -12,8 +13,10 @@ class DeviceListingPage extends StatefulWidget {
 
 class _DeviceListingPageState extends State<DeviceListingPage> {
   final flutterBlue = FlutterBlue.instance;
+  final LocalStorage storage = LocalStorage('bluetooth');
 
   List<ScanResult> scanResults = [];
+  Map<String, String> deviceNames = {};
   bool loading = false;
 
   @override
@@ -23,9 +26,17 @@ class _DeviceListingPageState extends State<DeviceListingPage> {
   }
 
   Future<void> listenDevices() async {
+    await storage.ready;
+    final devices = storage.getItem('devices');
+
+    if (devices != null) {
+      deviceNames = Map<String, String>.from(devices);
+    }
+
     await Permission.bluetooth.request();
     await Permission.bluetoothScan.request();
     await Permission.bluetoothConnect.request();
+
     setState(() {
       loading = true;
     });
@@ -51,17 +62,23 @@ class _DeviceListingPageState extends State<DeviceListingPage> {
             children: scanResults
                 .map(
                   (result) => ListTile(
-                    title: Text(
-                      result.device.name.isEmpty
-                          ? 'Dispositivo desconhecido ${scanResults.indexOf(result)}'
-                          : result.device.name,
-                    ),
+                    title: Text(deviceNames[result.device.id.toString()] ??
+                        'Dispositivo desconhecido'),
                     subtitle: Text(result.device.id.toString()),
                     trailing: const Icon(Icons.keyboard_arrow_right),
                     onTap: () => Navigator.of(context).push(
                       MaterialPageRoute(
                         builder: (context) => DeviceDetailsPage(
                           device: result.device,
+                          deviceNames: deviceNames,
+                          onChangeDeviceName: (name) {
+                            deviceNames = {
+                              ...deviceNames,
+                              result.device.id.toString(): name,
+                            };
+                            storage.setItem('devices', deviceNames);
+                            setState(() {});
+                          },
                         ),
                       ),
                     ),
